d2f5d34fc641e85ff69c4c6eff3b810d38005aaf
admin <ned.martorell@dataiku.com>
2020-12-08 09:56:07 -0500

    Saved recipe: GENERICSUPPORT.compute_output2

---
+++ recipes/compute_output2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+avocado_transactions_partitioned2_prepared = dataiku.Dataset("avocado_transactions_partitioned2_prepared")
+avocado_transactions_partitioned2_prepared_df = avocado_transactions_partitioned2_prepared.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+output2_df = avocado_transactions_partitioned2_prepared_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+output2 = dataiku.Dataset("output2")
+output2.write_with_schema(output2_df)
